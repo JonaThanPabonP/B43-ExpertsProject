ea39c181776ab95610d976e3db93bebc875fa08d
Jonathan Pabón Prieto <71296819+JonaThanPabonP@users.noreply.github.com>
2021-11-08 21:28:17 -0500

    Test get-user

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'))
 const db = require('./src/db/crudExperts.js');
+
+
+app.get('/', function (req, res) {
+    res.send('Servidor Experts con Express')
+});
+
+app.get('/nueva-ruta', (req,res)=>{
+    res.send('Esta es una nueva ruta en el servidor de Express');
+})
+
+app.get('/get-expert/:id', (req,res)=>{
+    const eid = req.params.id;
+    res.send('Se consultó el documento '+eid);
+})
+
+app.get('/get-user', (req,res)=>{
+    res.json({
+        "name": "Carolina",
+        "DNI": 8646441
+    });
+})
+
+
+app.listen(port, ()=>{
+    console.log('My port is listening', port);
+});
