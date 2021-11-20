de808685080fa72f5661bc9fa1ad6941c20fa90a
Jonathan Pabón Prieto <71296819+JonaThanPabonP@users.noreply.github.com>
2021-11-19 21:59:12 -0500

    my app
    Mi primera app con reactjs
    Limpiando el entorno
    Creación de componentes e importación en el App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ButtonOK from './components/ButtonOK';
+import NavBar from './components/NavBar';
+
+
+function App() {
+    return (
+      <React.Fragment>
+        <h1>Estudiantes MINTIC</h1>
+        <h1>Grupo B43</h1>
+        <NavBar/>
+        <ButtonOK/>
+      </React.Fragment>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ButtonOK from './components/ButtonOK';
+
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ul>
+        <ButtonOK/>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
